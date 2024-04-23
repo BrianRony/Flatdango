5344ecdec443b610157ce2ce30f710a693303efd
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-23 15:56:20 +0300

    Bonus  Deliverables

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
+document.addEventListener("DOMContentLoaded", ()=> {
 const baseUrl = "http://localhost:3000/films"
 
-
-//get movie names
+// Function to get movie names
 function getNames(){
     return fetch(baseUrl)
     .then(res => res.json())
 }
 
+// Function to render movie names
 function renderNames(movie){
     let listF = document.getElementById("films")
     let list = document.createElement("li")
-    listF.appendChild(list)
-    list.innerText = movie.title
+    list.innerText = (movie.title).toUpperCase()
     list.dataset.id = movie.id
     list.addEventListener("click", onListMovieClick)
+    listF.appendChild(list)
 }
+
 getNames().then(movies => movies.forEach(movie => renderNames(movie)))
 
-
-//get movie details
+// Function to get movie details
 function getMovieDetails(id){
    return fetch(baseUrl + `/${id}`)
    .then(res => res.json())
 }
+
+// Function to handle movie list item click
 function onListMovieClick(e){
     e.preventDefault()
     const movieId = e.target.dataset.id
@@ -30,6 +33,7 @@ function onListMovieClick(e){
     .then(renderMovieDetails)
 }
 
+// Function to render movie details
 function renderMovieDetails(movie){
     const poster = document.getElementById("poster")
     poster.src = movie.poster
@@ -42,23 +46,44 @@ function renderMovieDetails(movie){
     const info = document.getElementById("film-info")
     info.innerText = movie.description
     const tickets = document.getElementById("ticket-num")
-    tickets.innerText = parseInt(movie.capacity) - parseInt(movie.tickets_sold)  
-}
+    tickets.innerText = parseInt(movie.capacity) - parseInt(movie.tickets_sold) 
 
-//Update ticket information
-const button = document.getElementById("buy-ticket")
+ // Event listener for the "Buy Ticket" button
+    const button = document.getElementById("buy-ticket")
     button.addEventListener("click", (e)=> {
-    e.preventDefault()
-    const tickets = document.getElementById("ticket-num")
-    if(tickets.innerText > 0){
-    tickets.innerText = (parseInt(tickets.innerText) -1)
+        e.preventDefault()
+    if (parseInt(tickets.innerText,10) > 0){
+        updateTicketInfo(movie.id, movie)
     } else {
         tickets.innerText = "Sold Out"
+        button.disabled = true
     }
-})
+    })
+}
+// Function to update ticket information on the server
+//Experiencing challenge with page reloading after updating ticket info on server
+function updateTicketInfo(id, movie){
+   return fetch(baseUrl + `/${id}`,{
+        method: "PUT",
+        headers: {
+            "content-type":"application/json",
+            "accept":"application/json"
+        },
+        body:JSON.stringify({
+            title: movie.title,
+            runtime: movie.runtime,
+            capacity: movie.capacity,
+            showtime: movie.showtime,
+            tickets_sold: movie.tickets_sold + 1,
+            description: movie.description,
+            poster: movie.poster
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    getNames()
-    getMovieDetails()
 })
 
